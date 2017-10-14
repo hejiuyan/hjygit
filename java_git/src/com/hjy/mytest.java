4d1134ecb1caba711270c113230395a500579311
hejiuyan <2353989110@qq.com>
2017-10-14 12:30:42 +0800

    初次提交

---
+++ java_git/src/com/hjy/mytest.java
@@ -0,0 +1,13 @@
+package com.hjy;
+
+public class mytest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÄãºÃ");
+	}
+
+}
